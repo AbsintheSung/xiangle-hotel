00576f268a8bdee058e13a1e8b324fc1d12d7275
AbsintheSung <h90243768@gmail.com>
2024-11-18 11:13:12 +0800

    feat:添加tailwind的顏色、斷點等-基礎配置

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,51 @@ export default <Partial<Config>>{
     './app.vue',
   ],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      padding: '0.75rem', //16*0.75=12
+    },
+    fontFamily: {
+      'Noto-Sans-TC': ['Noto Sans TC', 'sans-serif']
+    },
+    screens: {
+      'ssm': '375px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      '2xl': '1320px',
+    },
+    extend: {
+      colors: {
+        primary: {
+          120: "#7B6651", //Dark
+          base: "#BF9D7D", //Base
+          80: "#D0B79F",
+          60: "#E1D1C2",
+          40: "#F1EAE4",
+          Tint: "#FAF7F5",
+        },
+        success: {
+          120: "#299F65", //Dark
+          base: "#52DD7E", //Base
+          20: "#BCFBBD",
+          10: "#E8FEE7",
+        },
+        info: {
+          120: "#1D66AC", //Dark
+          base: "#3BADEF", //Base
+          20: "#B1EFFD",
+          10: "#E6FBFE",
+        },
+        alert: {
+          120: "#C22538", //Dark
+          base: "#DA3E51", //Base
+          20: "#F5CCD1",
+          10: "#FDECEF",
+        }
+      }
+    }
   },
   plugins: []
 }
