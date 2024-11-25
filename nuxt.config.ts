95909024bb5cc26b88129a57487ed736d211cb3a
AbsintheSung <h90243768@gmail.com>
2024-11-25 16:08:21 +0800

    feat:配置header區塊含RWD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon',
-  ],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt'],
   css: ['~/assets/scss/index.scss', '~/assets/tailwind.css',],
   vite: {
     css: {
--- pages/index.vue
+++ pages/index.vue
@@ -114,9 +114,88 @@ const slidePrev = () => {
 const slideNext = () => {
   roomSwiper.value?.$el.swiper.slideNext();
 };
+
+const isOpenMenu = ref(false)
+const handleMenu = ()=>{
+  isOpenMenu.value = true
+}
+const handleCloseMenu = ()=>{
+  isOpenMenu.value = false
+}
+
+
+const windowScroll = useWindowScroll()
+const headerY = ref(windowScroll.y)
+const isScrolled = ref(false)
+
+
+onMounted(() => {
+  watch(()=>headerY.value,(newHeaderY)=>{
+    newHeaderY > 0 ? isScrolled.value = true : isScrolled.value = false
+  },{immediate:true})
+})
+
 </script>
 
 <template>
+  
+  <header class="w-full fixed z-30 transition-all duration-300 ease-in-out" :class="{ 'bg-black': isScrolled }">
+    <div class="px-3 py-6 flex items-center justify-between md:px-20">
+      <div>
+        <TheSvg color="#ffffff" class="w-[196px] h-[72px]"  :svgSrc="logoWhite" svgId="logo-white"></TheSvg>
+      </div>
+      <ul class="hidden  items-center gap-x-4 font-bold text-white md:flex">
+        <li >
+          <RouterLink class="block p-4" to="/">
+            客房旅宿
+          </RouterLink>    
+        </li>
+        <li >
+          <RouterLink class="block p-4" to="/">
+            會員登入
+          </RouterLink>
+        </li>
+        <li class="bg-primary-base rounded-xl">
+          <RouterLink class="block py-4 px-8" to="/">
+            立即訂房
+          </RouterLink>
+        </li>
+      </ul>
+      <div class="md:hidden">
+        <button>
+          <Icon class="text-white text-3xl" name="gridicons:menu" @click="handleMenu"></Icon>
+        </button>
+      </div>
+    </div>
+    <Transition name="fade">
+      <div v-if="isOpenMenu" class="p-5 fixed inset-0 w-full h-full z-50 bg-black flex flex-col items-center justify-center">
+        <div class="absolute top-5 right-5">
+          <button @click="handleCloseMenu">
+            <Icon class="text-white text-3xl" name="gridicons:cross"></Icon>
+          </button>
+        </div>
+        <div class="w-full">
+          <ul class="flex flex-col items-center gap-y-4 font-bold text-white">
+            <li class="w-full text-center">
+              <RouterLink class="block p-4 w-full" to="/">
+                客房旅宿
+              </RouterLink>
+            </li>
+            <li class="w-full text-center">
+              <RouterLink class="block p-4 w-full" to="/">
+                會員登入
+              </RouterLink>
+            </li>
+            <li class="w-full text-center bg-primary-base rounded-xl">
+              <RouterLink class="block p-4 w-full" to="/">
+                立即訂房
+              </RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </Transition>
+  </header>
   <main>
     <section class="relative">
       <Swiper v-bind="swiperConfig" class="h-full">
@@ -441,5 +520,15 @@ const slideNext = () => {
   // z-index: -1;
   background-image: url("@/assets/svg/deco-wave-bg.svg");
   background-repeat: no-repeat;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
