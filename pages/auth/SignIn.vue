c8779d3c75877509d66d13bb014f1c08a89d87a2
AbsintheSung <h90243768@gmail.com>
2024-11-28 17:17:22 +0800

    update:SignIn-添加部份樣式

--- pages/auth/SignIn.vue
+++ pages/auth/SignIn.vue
@@ -13,12 +13,12 @@
 
         <form class="w-full flex flex-col items-center gap-y-4 text-white sm:w-1/2">
           <div class="w-full flex flex-col gap-y-2">
-            <label>電子信箱</label>
-            <input class="p-4 text-black rounded-lg" />
+            <label class="font-bold">電子信箱</label>
+            <input class="p-4 text-black font-bold rounded-lg" />
           </div>
           <div class="w-full flex flex-col gap-y-2">
-            <label>密碼</label>
-            <input class="p-4 text-black rounded-lg" />
+            <label class="font-bold">密碼</label>
+            <input class="p-4 text-black font-bold rounded-lg" />
           </div>
           <div class="w-full flex justify-between items-center font-medium">
             <div class="flex items-end gap-2 text-neutral-0">
@@ -28,13 +28,13 @@
             <NuxtLink to="/" class="text-primary-base font-bold">忘記密碼?</NuxtLink>
           </div>
           <div class="w-full mt-6">
-            <button class="w-full py-4 bg-primary-base rounded-lg">會員登入</button>
+            <button class="w-full py-4 bg-primary-base font-bold rounded-lg">會員登入</button>
           </div>
         </form>
 
         <div class="w-full flex justify-start items-center gap-x-2 font-medium sm:w-1/2">
           <p class="text-white">沒有會員嗎?</p>
-          <NuxtLink to="/" class="text-primary-base">前往註冊</NuxtLink>
+          <NuxtLink to="/" class="font-bold text-primary-base">前往註冊</NuxtLink>
         </div>
       </div>
     </div>
