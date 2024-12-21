52ac3eddf2889b1cb5d3b40147b20e8ec54bab99
AbsintheSung <h90243768@gmail.com>
2024-12-21 11:32:46 +0800

    update:配置登入api以及邏輯顯示

--- composables/signInForm.ts
+++ composables/signInForm.ts
@@ -3,7 +3,22 @@ import { toTypedSchema } from "@vee-validate/zod";
 import { userFormSchema } from "~/pages/auth/schemas/useSigninForm";
 import type { UserFormSchema } from "~/pages/auth/schemas/useSigninForm";
 
+type SignInErrorResponse = {
+  status: boolean;
+  message: string;
+};
+
+type SignInError = {
+  response?: {
+    _data?: SignInErrorResponse;
+    status?: number;
+  };
+  message?: string;
+};
+
 export const useSignInForm = () => {
+  const config = useRuntimeConfig();
+  // const { $swal } = useNuxtApp();
   const validationSchema = toTypedSchema(userFormSchema);
   const initialValues: UserFormSchema = {
     email: "",
@@ -19,12 +34,43 @@ export const useSignInForm = () => {
   const password = useField<string>('password')
 
   const submitSigninForm = handleSubmit(
-    (values) => {
-      console.log("表單驗證成功", values);
-      resetForm(); // 初始化表單
+    async (values) => {
+      try {
+        await $fetch(`${config.public.apiBase}/api/v1/user/login`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: {
+            email: values.email,
+            password: values.password,
+          },
+          onResponse({ request, response, options }) {
+            const message = response._data;
+            const authCookie = useCookie(config.public.cookieName, { path: "/" })
+            authCookie.value = message.token
+          }
+        });
+        console.log('登入成功')
+        resetForm(); // 初始化表單
+
+      } catch (error) {
+        const signInError = error as SignInError;
+        throw new Error(
+          signInError.response?._data?.message
+        );
+        // $swal.fire({
+        //   position: "center",
+        //   icon: 'error',
+        //   title: '登入失敗',
+        //   text: signInError.response?._data?.message,
+        //   showConfirmButton: false,
+        //   timer: 1500
+        // });
+      }
     },
     (errors) => {
-      console.log("表單驗證失敗");
+      console.log("表單驗證失敗", errors);
     }
   );
   return {
--- pages/auth/components/SigninForm.vue
+++ pages/auth/components/SigninForm.vue
@@ -1,32 +1,60 @@
 <script setup lang="ts">
 import { useSignInForm } from "~/composables/signInForm";
 const { errors, email, password, submitSigninForm } = useSignInForm();
+const isLoading = ref<boolean>(false);
+const handleSignin = async (): Promise<void> => {
+  isLoading.value = true;
+  try {
+    await submitSigninForm();
+  } catch (error) {
+    console.log(error);
+    const errorMessage = (error as Error).message;
+    const { $swal } = useNuxtApp();
+    $swal.fire({
+      position: "center",
+      icon: "error",
+      title: "登入失敗",
+      text: errorMessage,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } finally {
+    isLoading.value = false;
+  }
+};
 </script>
 <template>
-  <form @submit="submitSigninForm" class="w-full flex flex-col items-center text-white sm:w-2/3 4xl:w-1/2">
+  <form class="w-full flex flex-col items-center text-white sm:w-2/3 4xl:w-1/2">
     <div class="relative w-full pb-6 flex flex-col gap-y-2">
       <label class="font-bold">電子信箱</label>
-      <input name="email" type="email" v-model="email" class="p-4 text-black font-bold rounded-lg" />
+      <input name="email" type="email" v-model="email" class="p-4 text-black font-bold rounded-lg" :disabled="isLoading" />
       <Transition name="error-mes-anime">
         <span v-if="errors.email !== undefined" class="absolute right-0 bottom-0 font-bold text-[16px] text-alert-base">{{ errors.email }}</span>
       </Transition>
     </div>
     <div class="relative w-full pb-6 flex flex-col gap-y-2">
       <label class="font-bold">密碼</label>
-      <input name="password" v-model="password" class="p-4 text-black font-bold rounded-lg" />
+      <input name="password" type="password" v-model="password" class="p-4 text-black font-bold rounded-lg" :disabled="isLoading" />
       <Transition name="error-mes-anime">
         <span v-if="errors.password !== undefined" class="absolute right-0 bottom-0 font-bold text-[16px] text-alert-base">{{ errors.password }}</span>
       </Transition>
     </div>
     <div class="py-4 w-full flex justify-between items-center font-medium">
       <div class="flex items-end gap-2 text-neutral-0">
-        <input id="remember" type="checkbox" class="remember-checkbox" />
+        <input id="remember" type="checkbox" class="remember-checkbox" :disabled="isLoading" />
         <label for="remember" class="font-bold cursor-pointer">記住帳號</label>
       </div>
       <NuxtLink to="/" class="text-primary-base font-bold">忘記密碼?</NuxtLink>
     </div>
     <div class="w-full mt-6">
-      <button type="submit" class="w-full py-4 bg-primary-base font-bold rounded-lg">會員登入</button>
+      <button class="w-full py-4 bg-primary-base font-bold rounded-lg" @click.prevent="handleSignin">
+        <p class="relative w-fit mx-auto">
+          會員登入
+          <span v-show="isLoading" class="absolute top-1/2 right-0 translate-x-full -translate-y-1/2 flex items-center justify-center">
+            <Icon name="svg-spinners:270-ring-with-bg" />
+          </span>
+        </p>
+      </button>
     </div>
   </form>
 </template>
