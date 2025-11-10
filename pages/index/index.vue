b31cf9b431b1391e5a951a2d3d7b8dcd8960b57f
AbsintheSung <h90243768@gmail.com>
2025-11-10 23:37:15 +0800

    update:將首頁美食輪播改成內部組件

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,6 +2,7 @@
 import homeHeroImg from "@/assets/images/desktop/home-hero.png";
 import homeHeroImgSm from "@/assets/images/mobile/home-hero-sm.png";
 import RoomsSwiper from "./components/RoomsSwiper.vue";
+import CulinarySwiper from "./components/CulinarySwiper.vue";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { homeMain, homeCulinary } from "@/utils/swiperConfigs";
 // 引入所需的 Swiper 樣式
@@ -37,7 +38,7 @@ const getCulinaryDataList = computed(() => {
       time,
     };
   });
-  return data;
+  return data ?? []; //  如果是 undefined 就返回空陣列
 });
 
 const getRoomsData = computed(() => {
@@ -167,27 +168,7 @@ const handleNext = (_val: number) => {
           </h2>
           <div class="w-full h-[2px] bg-gradient-to-r from-[#BE9C7C] to-white sm:w-1/6" />
         </div>
-        <Swiper v-bind="homeCulinary">
-          <SwiperSlide v-for="(culinaryItem, index) in getCulinaryDataList" :key="culinaryItem._id">
-            <div class="relative overflow-hidden">
-              <picture>
-                <img class="w-full object-cover min-h-[400px] max-h-[600px] transition-transform duration-300 ease-in-out hover:scale-110" :src="culinaryItem.image" :alt="culinaryItem.title" />
-              </picture>
-              <div class="absolute bottom-0 p-4 flex flex-col gap-y-4 text-white backdrop-blur-sm md:p-6 md:gap-y-6">
-                <div class="font-bold text-nowrap flex items-center justify-between">
-                  <h3 class="text-2xl">{{ culinaryItem.title }}</h3>
-                  <p class="flex items-center gap-x-2 md:gap-x-4">
-                    <time>{{ culinaryItem.month }}</time>
-                    <time>{{ culinaryItem.time }}</time>
-                  </p>
-                </div>
-                <div>
-                  <p>{{ culinaryItem.description }}</p>
-                </div>
-              </div>
-            </div>
-          </SwiperSlide>
-        </Swiper>
+        <CulinarySwiper class="home-culinary-swiper" :getCulinaryDataList="getCulinaryDataList" />
       </div>
     </section>
     <section class="relative bg-black py-20 md:py-28">
